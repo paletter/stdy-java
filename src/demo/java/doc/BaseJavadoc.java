717d208ac740e8607491f75c1b4d520c859eff60
paletter <234866877@qq.com>
2016-10-13 14:41:40 +0800

    EX

---
+++ src/demo/java/doc/BaseJavadoc.java
@@ -0,0 +1,25 @@
+package demo.java.doc;
+
+/**
+ *  <p>BaseJavadoc</p>
+ *  
+ *  Test to generate Javadoc(Class)
+ *  
+ * 	@author fangbo
+ * 	@version 1.0
+ *
+ */
+public class BaseJavadoc {
+
+	/**
+	 * BaseJavadoc.method1
+	 * 
+	 * Test to generate Javadoc(Method)
+	 * 
+	 * @param arg
+	 * @return null
+	 */
+	public String method1(String arg) {
+		return null;
+	}
+}
